4fc08241f7af1fd6b03f819970e82d750f42e730
tubaranasakar <tubaranasakar@hotmail.com>
2024-11-11 22:52:14 +0300

    BottomSheet kullanımı eklendi

--- app/src/main/java/com/example/navigationcomponentkullanimi/AnasayfaFragment.kt
+++ app/src/main/java/com/example/navigationcomponentkullanimi/AnasayfaFragment.kt
@@ -1,6 +1,5 @@
 package com.example.navigationcomponentkullanimi
 
-import android.health.connect.datatypes.units.Length
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,9 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
-import androidx.navigation.Navigator
 import com.example.navigationcomponentkullanimi.databinding.FragmentAnasayfaBinding
-import com.google.android.material.snackbar.Snackbar
 
 class AnasayfaFragment : Fragment() {
     private lateinit var binding: FragmentAnasayfaBinding
